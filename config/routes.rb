96aae9f72ee4d4a43df7cae71c135267cb9c2101
Yoshi3lines <ny3line@gmail.com>
2021-11-25 06:36:38 +0900

    p92 comp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+  resources :publishers
+  resource :profile, only: %i(show, edit update)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
